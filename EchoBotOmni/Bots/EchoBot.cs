124544a137b019f744f0239a5fa866a063440818
Dušan Jovičić <dusan.jovicic@comtrade.com>
2023-01-13 17:15:55 +0100

    init

---
+++ EchoBotOmni/Bots/EchoBot.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace EchoBotOmni.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        private readonly ILogger _logger;
+        public EchoBot(ILogger<EchoBot> logger)
+        {
+            _logger = logger;
+        }
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            Debug.WriteLine("Message received: " + turnContext.Activity.Text + " || " + DateTime.Now);
+            _logger.LogCritical("Message received: " + turnContext.Activity.Text + " || " + DateTime.Now);
+            
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                IActivity replyActivity = null;
+                if (turnContext.Activity.Text.StartsWith("{"))
+                {
+                     replyActivity = MessageFactory.Text($"Welcome User");
+                }
+                else {
+                     replyActivity = MessageFactory.Text($"Echoo: {turnContext.Activity.Text}");
+
+                    // Replace with your own condition for bot escalation
+                    if (turnContext.Activity.Text.Equals("escalate", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Debug.WriteLine("Escalate triggered");
+                        _logger.LogDebug("Escalate triggered");
+
+                        Dictionary<string, object> contextVars = new Dictionary<string, object>() { { "BotHandoffTopic", "CreditCard" } };
+                        OmnichannelBotClient.AddEscalationContext(replyActivity, contextVars);
+                    }
+                    // Replace with your own condition for bot end conversation
+                    else if (turnContext.Activity.Text.Equals("endconversation", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        _logger.LogDebug("End conversation triggered");
+                        Debug.WriteLine("End conversation triggered");
+
+                        OmnichannelBotClient.AddEndConversationContext(replyActivity);
+                    }
+                    // Call method BridgeBotMessage for every response that needs to be delivered to the customer.
+                    else
+                    {
+                        var time = OmnichannelBotClient.BridgeBotMessage(replyActivity);
+                        Debug.WriteLine("BridgeBotMessage time: " + time);
+                        _logger.LogCritical("BridgeBotMessage time: " + time);
+                    }
+                }
+                _logger.LogCritical("Message response: " + turnContext.Activity.Text + " || " + DateTime.Now);
+                Debug.WriteLine("Message response: " + turnContext.Activity.Text + " || " + DateTime.Now);
+
+                await turnContext.SendActivityAsync(replyActivity, cancellationToken);
+            }
+        }
+
+        /// <summary>
+        /// This method is called when there is a participant added to the chat.
+        /// </summary>
+        /// <param name="membersAdded">Member being added to the chat</param>
+        /// <param name="turnContext">TurnContext</param>
+        /// <param name="cancellationToken">CancellationToken</param>
+        /// <returns></returns>
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    //Set the bridge mode for every message that needs to be delivered to customer
+                    OmnichannelBotClient.BridgeBotMessage(turnContext.Activity);
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Welcome to Echo Bot."), cancellationToken);
+                }
+            }
+        }
+    }
+}
---
+++ EchoBotOmni/OmnichannelBotClient.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using EchoBotOmni.Command;
+using System.Diagnostics;
+
+namespace EchoBotOmni
+{
+    public static class OmnichannelBotClient
+    {
+        /// <summary>
+        /// Delivery mode of bot's reply activity
+        /// </summary>
+        private const string DeliveryMode = "deliveryMode";
+        /// <summary>
+        /// Delivery Mode value bridged
+        /// </summary>
+        private const string Bridged = "bridged";
+        /// <summary>
+        /// Custom data tag
+        /// </summary>
+        private const string Tags = "tags";
+
+        /// <summary>
+        /// Adds Omnichannel for Customer Service escalation context to the bot's reply activity.
+        /// </summary>
+        /// <param name="activity">Bot's reply activity</param>
+        /// <param name="contextVars">Omnichannel for Customer Service Workstream context variable value pairs</param>
+        public static void AddEscalationContext(IActivity activity, Dictionary<string, object> contextVars)
+        {
+            Command.Command command = new Command.Command
+            {
+                Type = CommandType.Escalate,
+                Context = contextVars
+            };
+
+            string serializedString = JsonConvert.SerializeObject(command);
+            if (activity.ChannelData != null)
+            {
+                (activity as IActivity).ChannelData[Tags] = serializedString;
+            }
+            else
+            {
+                activity.ChannelData = new Dictionary<string, object>() { { Tags, serializedString } };
+            }
+        }
+
+        /// <summary>
+        /// Adds Omni-channel end conversation context to the bot's reply activity.
+        /// </summary>
+        /// <param name="activity">Bot's reply activity</param>
+        public static void AddEndConversationContext(IActivity activity)
+        {
+            Command.Command command = new Command.Command
+            {
+                Type = CommandType.EndConversation,
+                Context = new Dictionary<string, object>()
+            };
+
+            string serializedString = JsonConvert.SerializeObject(command);
+            if (activity.ChannelData != null)
+            {
+                (activity as IActivity).ChannelData[Tags] = serializedString;
+            }
+            else
+            {
+                activity.ChannelData = new Dictionary<string, object>() { { Tags, serializedString } };
+            }
+        }
+
+        /// <summary>
+        /// Sets delivery mode for bot as bridged so that Customer can see bot messages
+        /// </summary>
+        /// <param name="activity">Bot's reply activity</param>
+        public static string BridgeBotMessage(IActivity activity)
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            if (activity.ChannelData != null)
+            {
+                (activity as IActivity).ChannelData[DeliveryMode] = Bridged;
+            }
+            else
+            {
+                activity.ChannelData = new Dictionary<string, object>() { { DeliveryMode, Bridged } };
+            }
+            s.Stop();
+            return s.Elapsed.ToString();
+        }
+    }
+}
